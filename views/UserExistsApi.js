309fcf46c34c4a5d9aabf4a477f556007a6b9b39
神兰科 <liujunhao173905@gmail.com>
2025-12-22 18:22:26 +0800

    Delete views directory

--- views/UserExistsApi.js
+++
@@ -1,6 +0,0 @@
-// 用于辅助前端判断用户名是否存在的API调用
-import axios from 'axios';
-
-export async function checkUserExists(username) {
-  return axios.get(`/api/user_exists/?username=${encodeURIComponent(username)}`);
-}
